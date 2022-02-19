007f4db55d0302f4c543a33d6460ec15bcf8cee6
Rafael1108 <edwin.larreab@ug.edu.ec>
2022-02-19 01:28:44 -0500

    Correcion del modelo

---
+++ database/migrations/2022_02_19_055515_create_usuarios_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('usuarios', function (Blueprint $table) {
+            $table->id();
+            $table->enum('TipoRol', ['1', '2', '3', '4'])->comment('Rol_Usuario (1=>Administrador, 2=>Gerente, 3=>Mecanico, 4=>Vendedor)');
+            $table->string('NickName', 15);
+            $table->string('Email', 25)->nullable()->default(null)->comment('Email_Usuario');
+            $table->string('NombreCompleto', 30);
+            $table->binary('PasswordSALT');
+            $table->binary('PasswordHASH');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('usuarios');
+    }
+};
