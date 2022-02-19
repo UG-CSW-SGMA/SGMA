a030e35e320d4eb20f9ef31e22dd925ad8c1c137
Rafael1108 <edwin.larreab@ug.edu.ec>
2022-02-19 12:20:50 -0500

    log accesos

---
+++ database/migrations/2022_02_19_161318_create_log_accesos_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('log_accesos', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('UsuarioId');
+            $table->dateTime('FechaHoraAcceso');
+            $table->string('Nombre_Navegador', 100);
+            $table->string('IP', 17);
+            $table->string('InfoAdicional', 255);
+
+            $table->foreign('UsuarioId')->references('Id')->on('usuarios');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('log_accesos');
+    }
+};
