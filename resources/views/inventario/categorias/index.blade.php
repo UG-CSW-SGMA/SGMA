90239a138adf95a44f6ff23c5a6b97f1179ad0f8
Rafael1108 <edwin.larreab@ug.edu.ec>
2022-02-20 02:11:45 -0500

    datatable

--- resources/views/inventario/categorias/index.blade.php
+++ resources/views/inventario/categorias/index.blade.php
@@ -3,6 +3,7 @@
 @section ('css')
 <link href="https://cdn.datatables.net/1.11.4/css/dataTables.bootstrap5.min.css" rel="stylesheet">
 @endsection
+
 @section('contenido')
 <!-- Page Heading -->
 <!-- DataTales Example -->
@@ -39,4 +40,15 @@
         </div>
     </div>
 </div>
+@section ('js')
+<script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+<script src="https://cdn.datatables.net/1.11.4/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.11.4/js/dataTables.bootstrap5.min.js"></script>
+<script>
+    $(document).ready(function() {
+        $('#example').DataTable();
+    });
+</script>
+@endsection
+
 @endsection
