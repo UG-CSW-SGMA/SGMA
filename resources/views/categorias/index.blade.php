47a56277bf2bf1bb042d4d9adc3d5f17b9bf922b
Rafael1108 <edwin.larreab@ug.edu.ec>
2022-02-19 21:53:14 -0500

    Implementacion CSS para personalizaciones.

--- resources/views/categorias/index.blade.php
+++ resources/views/categorias/index.blade.php
@@ -2,14 +2,13 @@
 
 @section('contenido')
 <!-- Page Heading -->
-<div class="d-sm-flex align-items-center justify-content-between mb-4">
-    <h1 class="h3 mb-2 text-gray-800"></h1>
-    <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm" data-toggle="modal" data-target="#newClientModal"><i class="fas fa-plus-circle fa-sm text-white-50"></i> Nueva Categoría</a>
-</div>
 <!-- DataTales Example -->
 <div class="card shadow mb-4">
     <div class="card-header py-3">
-        <h6 class="m-0 font-weight-bold text-primary">Categorías</h6>
+        <div class="d-sm-flex align-items-center justify-content-between mb-4">
+            <h6 class="m-0 font-weight-bold text-primary">Categorías</h6>
+            <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm" data-toggle="modal" data-target="#newClientModal"><i class="fas fa-plus-circle fa-sm text-white-50"></i> Nueva Categoría</a>
+        </div>
     </div>
     <div class="card-body">
         <div class="table-responsive">
@@ -18,7 +17,7 @@
                     <tr>
                         <th scope="col">Nombre</th>
                         <th scope="col">Descripcón</th>
-                        <th scope="col">-</th>
+                        <th scope="col"></th>
                     </tr>
                 </thead>
                 <tbody>
--- resources/views/vistapadre.blade.php
+++ resources/views/vistapadre.blade.php
@@ -18,6 +18,8 @@
     <!-- Custom styles for this template-->
     <link href="{{ asset('css/sb-admin-2.min.css')}}" rel="stylesheet">
 
+    <!-- css personalizado-->
+    @yield('css')
 
 
 </head>
