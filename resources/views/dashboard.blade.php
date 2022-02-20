6ce87283accc58e2c15757670c52f5cc9eb8bc63
Rafael1108 <edwin.larreab@ug.edu.ec>
2022-02-20 01:06:58 -0500

    ok

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,4 @@
+@extends('main/main')
+
+@section('contenido')
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('vistapadre');
+    return view('dashboard');
 });
 
 
