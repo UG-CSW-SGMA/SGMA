74df49464d4e5e12f734c712551a7b1024be01ae
edgarbasurto <edgar.basurtoc@ug.edu.ec>
2022-02-21 00:41:03 -0500

    modal create vehiculos

--- resources/views/taller/vehiculos/create.blade.php
+++ resources/views/taller/vehiculos/create.blade.php
@@ -1,32 +1,38 @@
-@extends('main.main')
+@extends('main.layout')
+
+@section('form-title')
+<h6 class="m-0 font-weight-bold text-primary">Nuevo Vehículo</h6>
+@endsection
+
 
 @section('contenido')
-<h2>CREAR NUEVO VEHÍCULO</h2>
 
 <form action="/articulos" method="post">
-<div class="mb-3">
-    <label for="" class="form-label">Código</label>
-    <input id="codigo" name="codigo" type="text" class="form-control" tabindex="1">
-</div>
+    <div class="mb-3">
+        <label for="" class="form-label">Código</label>
+        <input id="codigo" name="codigo" type="text" class="form-control" tabindex="1">
+    </div>
 
-<div class="mb-3">
-    <label for="" class="form-label">Descripción</label>
-    <input id="descripcion" name="descripcion" type="text" class="form-control" tabindex="2">
-</div>
+    <div class="mb-3">
+        <label for="" class="form-label">Descripción</label>
+        <input id="descripcion" name="descripcion" type="text" class="form-control" tabindex="2">
+    </div>
 
-<div class="mb-3">
-    <label for="" class="form-label">Cantidad</label>
-    <input id="cantidad" name="cantidad" type="number" class="form-control" tabindex="3">
-</div>
+    <div class="mb-3">
+        <label for="" class="form-label">Cantidad</label>
+        <input id="cantidad" name="cantidad" type="number" class="form-control" tabindex="3">
+    </div>
 
-<div class="mb-3">
-    <label for="" class="form-label">Precio</label>
-    <input id="precio" name="precio" type="number" step="any" value="0.0" class="form-control" tabindex="4">
-</div>
+    <div class="mb-3">
+        <label for="" class="form-label">Precio</label>
+        <input id="precio" name="precio" type="number" step="any" value="0.0" class="form-control" tabindex="4">
+    </div>
+
+    <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+        <button type="submit" class="btn btn-primary" name="action" id="#" value="add" data-dismiss="modal">Guardar</button>
+    </div>
 </form>
 
 
 @endsection
-
-
-
--- resources/views/taller/vehiculos/vehiculos.blade.php
+++ resources/views/taller/vehiculos/vehiculos.blade.php
@@ -11,7 +11,7 @@
 <!-- Page Heading -->
 <div class="d-sm-flex align-items-center justify-content-between mb-4">
     <h1 class="h3 mb-2 text-gray-800">Vehículos</h1>
-    <a href="vehiculos/create" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i class="fas fa-plus-circle fa-sm text-white-50"></i> Nuevo Vehículo</a>
+    <a class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm" data-toggle="modal" id="mediumButton" data-target="#mediumModal" data-attr="vehiculos/create" title="Crear Nuevo Vehículo"><i class="fas fa-plus-circle fa-sm text-white-50"></i> Nuevo Vehículo</a>
 </div>
 
 <!-- DataTales Example -->
@@ -35,7 +35,7 @@
 
                     </tr>
                 </thead>
-               
+
                 <tbody>
                     @foreach($vehiculos as $vehiculo)
                     <tr>
@@ -47,8 +47,8 @@
                         <td>{{$vehiculo->Color}}</td>
                         <td>{{$vehiculo->Descripcion}}</td>
                         <td>
-                            <a href="{{$vehiculo->Id}}" class="d-none d-sm-inline btn btn-sm btn-primary shadow-sm"><i class="fas fa-edit fa-sm text-white-50"></i></a>
-                            <a href="{{$vehiculo->Id}}" class="d-none d-sm-inline btn btn-sm btn-primary shadow-sm bg-gradient-danger"><i class="fas fa-times-circle fa-sm text-white-50 bg-gradient-danger"></i></a>
+                            <a href="vehiculos/edit({{$vehiculo->Id}})" class="d-none d-sm-inline btn btn-sm btn-primary shadow-sm"><i class="fas fa-edit fa-sm text-white-50"></i></a>
+                            <a href="vehiculos/destroy({{$vehiculo->Id}})" class="d-none d-sm-inline btn btn-sm btn-primary shadow-sm bg-gradient-danger"><i class="fas fa-times-circle fa-sm text-white-50 bg-gradient-danger"></i></a>
                         </td>
                         @endforeach
                 </tbody>
@@ -57,11 +57,26 @@
     </div>
 </div>
 
+<!-- small modal -->
+<div class="modal fade" id="smallModal" tabindex="-1" role="dialog" aria-labelledby="smallModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-sm" role="document">
+        <div class="modal-content" id="smallBody">
+
+        </div>
+    </div>
+</div>
+
+
+<!-- medium modal -->
+<div class="modal fade" id="mediumModal" tabindex="-1" role="dialog" aria-labelledby="mediumModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content" id="mediumBody">
+
+        </div>
+    </div>
+</div>
+
 @endsection
-
-
-
-
 
 @section ('js')
 <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
@@ -88,6 +103,58 @@
         });
 
         table.buttons().container().appendTo('#example_wrapper .col-md-6:eq(0)');
+    });
+
+    // display a modal (small modal)
+    $(document).on('click', '#smallButton', function(event) {
+        event.preventDefault();
+        let href = $(this).attr('data-attr');
+        $.ajax({
+            url: href,
+            beforeSend: function() {
+                $('#loader').show();
+            },
+            // return the result
+            success: function(result) {
+                //$('#smallModal').modal("show"); //Rafael1108 error si está disponible
+                $('#smallBody').html(result).show();
+            },
+            complete: function() {
+                $('#loader').hide();
+            },
+            error: function(jqXHR, testStatus, error) {
+                console.log(error);
+                alert("Page " + href + " cannot open. Error:" + error);
+                $('#loader').hide();
+            },
+            timeout: 8000
+        })
+    });
+
+    // display a modal (medium modal)
+    $(document).on('click', '#mediumButton', function(event) {
+        event.preventDefault();
+        let href = $(this).attr('data-attr');
+        $.ajax({
+            url: href,
+            beforeSend: function() {
+                $('#loader').show();
+            },
+            // return the result
+            success: function(result) {
+                //$('#mediumModal').modal("show"); //Rafael1108 error si está disponible
+                $('#mediumBody').html(result).show();
+            },
+            complete: function() {
+                $('#loader').hide();
+            },
+            error: function(jqXHR, testStatus, error) {
+                console.log(error);
+                alert("Page " + href + " cannot open. Error:" + error);
+                $('#loader').hide();
+            },
+            timeout: 8000
+        })
     });
 </script>
 
