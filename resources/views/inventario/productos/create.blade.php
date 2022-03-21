a3af64b3513b917ec89c71ff1f500ed9823b9bea
AndresPradoVelarde <67911568+AndresPradoVelarde@users.noreply.github.com>
2022-03-21 11:27:55 -0500

    Merge branch 'developer' of https://github.com/UG-CSW-SGMA/SGMA into developer

--- resources/views/inventario/productos/create.blade.php
+++ resources/views/inventario/productos/create.blade.php
@@ -5,6 +5,44 @@
 @endsection
 
 @section('contenido')
+<form action="productos" method="post">
+    @csrf
+    <div class="mb-3">
+        <label for="" class="form-label">Codigo</label>
+        <input id="codigo" name="codigo" type="text" class="form-control" tabindex="1" required>
+    </div>
+    <div class="mb-3">
+        <label for="" class="form-label">Categoria</label>
+        <input id="categoria" name="categoria" type="text" class="form-control" tabindex="1" required>
+    </div>
 
+    <div class="mb-3">
+        <label for="" class="form-label">Nombre</label>
+        <input id="nombre" name="nombre" type="text" class="form-control" tabindex="2" required>
+    </div>
+
+    <div class="mb-3">
+        <label for="" class="form-label">Descripción</label>
+        <input id="descripcion" name="descripcion" type="text" class="form-control" tabindex="3" required>
+    </div>
+
+    <div class="mb-3">
+        <label for="" class="form-label">Precio</label>
+        <input id="precio" name="precio" type="text" class="form-control" tabindex="4">
+    </div>
+
+    <div class="mb-3">
+        <label for="" class="form-label">Costo</label>
+        <input id="costo" name="costo" type="number" min="1900" max="2023" class="form-control" tabindex="5" required>
+    </div>
+
+    <div class="modal-footer">
+        <!-- <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button> -->
+        <!-- <button type="submit" class="btn btn-primary" name="action" id="#" value="add" data-dismiss="modal">Guardar</button> -->
+        <button type="button" class="btn btn-secondary" data-dismiss="modal" tabindex="8">Cerrar</button>
+        <button type="submit" class="btn btn-primary" tabindex="9">Guardar</button>
+    </div>
+
+</form>
 
 @endsection
