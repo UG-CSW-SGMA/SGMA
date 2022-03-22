c76e6d93e3f42c9d189ff009c73fe243f4f55598
edgarbasurto <edgar.basurtoc@ug.edu.ec>
2022-03-21 23:14:38 -0500

    sujeto cliente

--- app/Models/Sujeto.php
+++ app/Models/Sujeto.php
@@ -5,10 +5,31 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\DB;
+use PhpParser\Node\Stmt\Else_;
+
+use function PHPUnit\Framework\isEmpty;
+use function PHPUnit\Framework\isNull;
 
 class Sujeto extends Model
 {
     use HasFactory;
+
+    /**
+     * @edgarbasurto
+     * Método para obtener una vista del listado de clientes activos
+     */
+
+    public function getListadoClientes()
+    {
+        return DB::table('sujetos')
+            ->select('*')
+            ->where([
+                ['Activo', '=', '1'],
+                ['TipoSujeto', '=', '1'],
+            ])
+            ->get();
+    }
+
 
     /**
      * @Rafael1108
@@ -23,5 +44,18 @@ class Sujeto extends Model
                 ['TipoSujeto', '=', '2'],
             ])
             ->get();
+    }
+
+    public function getCliente($dni){
+        $encontrado = DB::table('sujetos')
+        ->select('*')
+        ->where('DNI', '=', $dni)
+        ->get();
+       
+        if (!($encontrado->isEmpty())) {
+            return TRUE;
+        } else {
+            return FALSE;
+        }
     }
 }
