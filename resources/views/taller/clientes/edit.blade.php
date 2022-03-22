7201a0c2d2fee63d8bb83bfa266b02ff1443bc8f
Rafael1108 <edwin.larreab@ug.edu.ec>
2022-03-22 02:05:08 -0500

    clientes edit ok

--- resources/views/taller/clientes/edit.blade.php
+++ resources/views/taller/clientes/edit.blade.php
@@ -5,7 +5,7 @@
 @endsection
 
 @section('contenido')
-<form action="clientes/{{cliente->id}}" method="POST">
+<form action="clientes/{{$cliente->id}}" method="POST">
     @csrf
     @method('PUT')
 
@@ -47,53 +47,5 @@
     </div>
 
 </form>
-
-
-
-
-<!-- <div class="mb-3">
-        <label for="txtTipoServicio" class="form-label">Tipo Servicio</label>
-        <select class="form-select" id="txtTipoServicio" name="txtTipoServicio" required value="{{$ObjCategoria->TipoServicioId}}">
-            @foreach($tiposServicios as $tp)
-            <option @if($ObjCategoria->TipoServicioId==$tp->id) selected @endif value="{{$tp->id}}">{{$tp->Nombre}}</option>
-            @endforeach
-
-        </select>
-        <div id="msjValidacion_Rol" class="invalid-feedback">
-            Escoja un Tipo Servicio.
-        </div>
-    </div>
-
-    <div class="mb-3    ">
-        <label for="txtNombre" class="form-label">Nombre</label>
-        <input type="text" class="form-control" id="txtNombre" name="txtNombre" maxlength="80" aria-describedby="msjValidacion_Nombre" placeholder="Nombre completo de la categoría" required value="{{$ObjCategoria->Nombre}}">
-        <div class="valid-feedback">
-            Datos correctos!
-        </div>
-        <div id="msjValidacion_Nombre" class="invalid-feedback">
-            Debe llenar el campo!.
-        </div>
-    </div>
-
-    <div class="mb-3    ">
-        <label for="txtDescripcion" class="form-label">Descripcion</label>
-        <input type="text" class="form-control" id="txtDescripcion" name="txtDescripcion" maxlength="80" aria-describedby="msjValidacion_Descrip" placeholder="Descripción corta" value="{{$ObjCategoria->Descripcion}}">
-        <div class="valid-feedback">
-            Datos correctos!
-        </div>
-        <div id="msjValidacion_Nombre" class="invalid-feedback">
-            Debe llenar el campo!.
-        </div>
-    </div>
-
-    <div class="modal-footer"> -->
-
-<!-- <button type="button" class="btn btn-secondary" data-dismiss="modal" tabindex="7">Cerrar</button>
-        <button type="submit" class="btn btn-primary" tabindex="8">Guardar</button>
-    </div>
-</form> -->
-
-
-
 
 @endsection
