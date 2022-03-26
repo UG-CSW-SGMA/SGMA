a27051e52afd7e60a539aeb87a975e3c9808a13e
Rafael1108 <edwin.larreab@ug.edu.ec>
2022-03-26 01:31:29 -0500

    reportEnding

--- resources/views/reportes/rptForm/rptNuevosClientes.blade.php
+++ resources/views/reportes/rptForm/rptNuevosClientes.blade.php
@@ -7,6 +7,7 @@
         table {
             font-family: arial, sans-serif;
             border-collapse: collapse;
+            font-size: 0.7em;
             width: 100%;
         }
 
@@ -30,6 +31,12 @@
             margin-top: 2px;
             margin-bottom: 2px;
         }
+
+        tfoot {
+            display: table-footer-group;
+            vertical-align: middle;
+            border-color: inherit;
+        }
     </style>
 </head>
 
@@ -40,22 +47,56 @@
         <h5>{{$emp->NombreComercial}}</h5>
         <h5>RUC: {{$emp->RUC}}</h5>
         <br>
-        <h4>REPORTE DE NUEVOS CLIENTES</h4>
-        <br>
+        <h3>REPORTE DE NUEVOS CLIENTES</h4>
+            <br>
     </div>
-    <div>
+    <br>
+    <div style="padding-bottom: 4px;">
         <?php echo $lblrango ?>
     </div>
     <table>
-        <tr>
-            <th>ID</th>
-            <th>Nommbre</th>
-        </tr>
-        <tr>
-            <td></td>
-            <td></td>
-        </tr>
+        <thead>
+            <tr>
+                <th>Fecha Creacion</th>
+                <th>DNI</th>
+                <th>Cliente</th>
+                <th>Teléfono</th>
+                <th>Email</th>
+                <th>Direccion</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($data as $row)
+            <tr>
+                @php $date = new DateTime($row->FechaCrecion);@endphp
+                <td>{{ $date->format('d-m-Y')}}</td>
+                <td>{{$row->DNI}}</td>
+                <td>{{$row->Nombre . ' ' . $row->Apellido}}</td>
+                <td>{{$row->Telefono}}</td>
+                <td>{{$row->Email}}</td>
+                <td>{{$row->Direccion}}</td>
+            </tr>
+            @endforeach
+        </tbody>
+        <tfoot>
+            <tr>
+                <td style="border: 0;"></td>
+                <td style="border: 0;"></td>
+                <td style="border: 0;"></td>
+                <td style="border: 0;"></td>
+                <td style="text-align: right;"><strong> Total de Registros =></strong></td>
+                <td>{{count($data)}}</td>
+            </tr>
+        </tfoot>
     </table>
+    <hr size="3" width="100%" color="black">
+    <div style="display: inline-flexbox; padding-bottom: 4px;">
+        @php
+        $fgenera = new DateTime();
+        $nombre_SS=Session('usuarioData')[0];
+        @endphp
+        <h6><strong> Generado por:</strong> {{$nombre_SS->NombreCompleto}} </h6>
+    </div>
 </body>
 
 </html>
