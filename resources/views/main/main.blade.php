d5f2cea125eeb28eecf9c3e0248dac71c1d7853a
Rafael1108 <edwin.larreab@ug.edu.ec>
2022-03-26 12:12:53 -0500

    ajuste en main para mostrar usuario

--- resources/views/main/main.blade.php
+++ resources/views/main/main.blade.php
@@ -336,9 +336,9 @@
                         <!-- Nav Item - User Information -->
                         <li class="nav-item dropdown no-arrow">
                             <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-
-                                <span class="mr-2 d-none d-lg-inline text-gray-600 small">{{Session('usuarionickname')}}</span>
-                                <img class="img-profile rounded-circle" src="img/undraw_profile.svg">
+                                @php $myseesion =Session('usuarioData')[0]@endphp
+                                <img class="img-profile rounded-circle my-2" src="img/undraw_profile.svg">
+                                <span class="ml-2 d-none d-lg-inline text-gray-600 small py-2"> <strong>{{$myseesion->NombreCompleto}}</strong></span>
                             </a>
                             <!-- Dropdown - User Information -->
                             <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
